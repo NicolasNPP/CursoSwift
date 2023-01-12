f67934d021e30b6e7285d5ca59b2588ca2e70676
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-01-12 13:14:47 -0300

    Secciones 1, 2, 3 y 4 del curso practicadas.

---
+++ CursoIos/Playgrounds/SeccionesDelCursoConPlaygrounds.playground/Pages/SeccionCuatro.xcplaygroundpage/Contents.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+//--Strings--
+
+//String en una linea
+
+let myString = "Sección 4 del curso"
+
+//String vacio
+var emptyString = ""
+var emptyString2 = String()
+
+//String en lineas múltiples
+
+let multipleString = """
+Bienvenidos al curso de Swift
+soy Nicolás Pepe
+"""
+//--Caracteres--
+
+//Caracter
+let myCaracter = "\u{E9}"
+let myCaracter2 = "\u{24}"
+
+//Caracter especial
+let myString2 = "Bienvenidos \nsoy Nicolás Pepe"
+
+// Emojis
+let myEmoji = "\u{1F496}"
+
+//--Concatenación e Interpolación--
+
+//Concatenación
+var myString3 = "Bienvenidos" + " " + "soy "
+myString3 += "Nicolás Pepe"
+
+//Interpolación
+let fullName = "Nicolás Pepe"
+let myPersonalInfo = "Mi nombre es \(fullName)"
+
+//--Comparación--
+
+let name1 = "Nico"
+let name2 = "Guillermo"
+let name3 = "Nico"
+let content = "Mi nombre es \(name1)"
+
+//No iguales - False
+name1 == name2
+
+//Iguales - True
+name1 == name3
+
+//Contenido
+content.contains(name1)
+
+//Vacía
+emptyString.isEmpty
+
+//Recorrido
+for valor in content {
+    print(valor)
+}
+
+
+
+
+
+
---
+++ CursoIos/Playgrounds/SeccionesDelCursoConPlaygrounds.playground/Pages/SeccionUnoDosYTres.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+//--Constantes y variables--
+
+//Variables
+var str = "Hello, playground"
+str = "Otro valor"
+
+//Constantes
+let stringConstante = "Una constante"
+
+/*
+ stringConstante = "Otro valor en constante"
+ No permite ya que es una constante y no una variable, habria que cambiarlo de let a var.
+*/
+
+//--Declaraciones múltiples--
+
+let myDate = "19-11-1994", myName = "Nicolás Pepe", myPlace = "Argentina", aInt = 0
+
+//--Tipos de datos comunes--
+
+//Enteros
+var x = 18
+
+//Double
+var y =  18.8
+
+//Caracteres aritméticos
+var addInt = x + 2
+var substractDouble = y - 2
+var multiplyInt = x * 2
+var divideInt = x / 2
+
+//Casteo
+var addCast = Double(x) + y
+
+//--Asignación explicita e implícita--
+
+var w: Double = 2.2
+var z: Int = 2
+var text: String = "Asignación explicita"
+
+
