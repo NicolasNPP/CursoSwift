7b3e1f0bd165c0857804368eba7e32999b491c46
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-01-17 15:21:34 -0300

    Sección 10 - Lección 86
    Cambio de Label con Button

--- CursoIos/ViewController.swift
+++ CursoIos/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelOne: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func buttonOne(_ sender: Any) {
+        labelOne.text = "Ya cambié"
+    }
+    
 }
 
