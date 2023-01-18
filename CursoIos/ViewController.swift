d6446793c4ba20ca429616ce5b34b7e9cdfdd708
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-01-18 10:18:38 -0300

    Clase 95 - Sección 12
    Buttons

--- CursoIos/ViewController.swift
+++ CursoIos/ViewController.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Oulets
     @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var buttonOne: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonOne.backgroundColor = .magenta
     }
 
     
     @IBAction func buttonOne(_ sender: Any) {
         labelOne.text = "Ya cambié"
+        buttonOne.backgroundColor = .black
     }
     
 }
