05a7046122a2645070330429d0d878d3b3a4ff48
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-01-18 11:23:18 -0300

    Clase 96 - Sección 12
    Pickers

--- CursoIos/ViewController.swift
+++ CursoIos/ViewController.swift
@@ -10,18 +10,42 @@ import UIKit
 class ViewController: UIViewController {
     
     //Oulets
-    @IBOutlet weak var labelOne: UILabel!
     @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var myPickerView: UIPickerView!
+    
+    //Variables
+    private let myPickerViewValues = ["Nicolás", "Alejandro", "Rosana", "Carlos", "Johan"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        buttonOne.backgroundColor = .magenta
+        
+        //Picker
+        myPickerView.backgroundColor = .lightGray
+        myPickerView.dataSource = self
+        myPickerView.delegate = self
     }
-
     
     @IBAction func buttonOne(_ sender: Any) {
-        labelOne.text = "Ya cambié"
-        buttonOne.backgroundColor = .black
+
     }
     
+}
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myPickerViewValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myPickerViewValues[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let myString = myPickerViewValues[row]
+        buttonOne.setTitle(myString, for: .normal)
+    }
 }
