c303a1ce87f967419d8275f5263603f00e0e81ad
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-01-20 11:37:05 -0300

    Clase 118 - Sección 14
    Maps

--- CursoIos/Seccion14/Seccion14ViewController.swift
+++ CursoIos/Seccion14/Seccion14ViewController.swift
@@ -1,16 +1,19 @@
 import UIKit
+import WebKit
+import MapKit
 
 class Seccion14ViewController: UIViewController {
-
-    @IBOutlet weak var myView1: UIView!
+    
+    @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var myMap: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-    }
-    
-    @IBAction func buttonAction(_ sender: Any) {
-        myView1.isHidden = true
+        //Web
+        myWebView.load(URLRequest(url: URL(string: "https://google.com")!))
+        
+        //Maps
+        myMap.centerCoordinate = CLLocationCoordinate2D(latitude: 42.70, longitude: -8.3)
     }
 }
