d1c99468a0e6e137367720387da507614028f20c
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-01-23 09:12:53 -0300

    Inicio sección 16 - Clase 132 y 133
    TableView y TableViewCell

--- CursoIos/Seccion16/TablasViewController.swift
+++ CursoIos/Seccion16/TablasViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class TablasViewController: UIViewController {
-    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var tView: UITableView!
+    
+    private let myCountrys = ["España","Mexico", "Peru", "Colombia", "Argentina"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tView.dataSource = self
+    }
+}
 
+extension TablasViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myCountrys.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+        if cell == nil {
+          cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        
+        cell!.textLabel?.text = myCountrys[indexPath.row]
+        
+        return cell!
     }
 }
