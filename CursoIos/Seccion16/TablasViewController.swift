9fd1959e617456b04f7aa58675f08d645633b8e3
Nicolas Pepe <Nicolasnahuel94@gmail.com>
2023-01-23 08:54:06 -0300

    Inicio de sección 15
    TableView

---
+++ CursoIos/Seccion16/TablasViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabasViewController.swift
+//  CursoIos
+//
+//  Created by Nicolas Pepe on 23/01/2023.
+//
+
+import UIKit
+
+class TablasViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
